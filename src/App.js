01a945b86dedf356df5af0265ef2ff354db56aa3
Mary Etokwudo <etokwudomary@gmail.com>
2019-08-22 00:10:39 +0100

    set up sign up route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import SignUp from './components/SignUp';
 import './App.css';
 
 function App() {
-  return (
-    <h1>Hi Decadevs</h1>
-  );
+	return (
+		<>
+			<Route path='/signup' exact component={SignUp} />
+		</>
+	);
 }
 
 export default App;
