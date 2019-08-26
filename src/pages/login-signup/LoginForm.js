4f75e82b9e5b2d5b860efca3fc00fb9bc1e27f52
Mary Etokwudo <etokwudomary@gmail.com>
2019-08-26 10:30:49 +0100

    add login functionality

--- src/pages/login-signup/LoginForm.js
+++ src/pages/login-signup/LoginForm.js
@@ -1,109 +1,114 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 
-import { signupValidator } from '../../utils/signupValidator';
+import { loginValidator } from '../../utils/loginValidator';
 import Button from '../../components/Button';
 import { Input, Label } from '../../components/Input';
-import '../../App.css';
 
 export default function LoginForm() {
-  const [errors, setErrors] = useState({});
+	const [errors, setErrors] = useState({});
 
-  const [isChecked, setIsChecked] = useState(false);
+	const [values, setValues] = useState({
+		email: '',
+		password: '',
+	});
 
-  const [values, setValues] = useState({
-    email: '',
-    password: ''
-  });
+	const onChange = ({ target: { name, value } }) => {
+		setValues({ ...values, [name]: value });
+		// remove the error display once a value is entered
+		setErrors({ ...errors, [name]: '' });
+	};
 
-  const onChange = ({ target: { name, value } }) => {
-    setValues({ ...values, [name]: value });
-    setErrors({ ...errors, [name]: '' });
-  };
+	const onSubmit = event => {
+		event.preventDefault();
 
-  const check = e => {
-    setIsChecked(e.target.checked);
-  };
+		const errorFields = loginValidator(values);
 
-  const onSubmit = event => {
-    event.preventDefault();
+		if (Object.keys(errorFields).length > 0) {
+			setErrors(errorFields);
+			return;
+		}
 
-    const { errorFields } = signupValidator(values, isChecked);
+		axios
+			.post('/public/login', values)
+			.then(res => {
+				if (res.data.statusCode !== 200) {
+					setErrors(res.data.errors);
+					alert(res.data.errors.error);
+				}
+				if (res.data.statusCode === 200 && res.data.token) {
+					const { payload, token } = res.data;
 
-    if (Object.keys(errorFields).length > 0) {
-      setErrors(errorFields);
-      return;
-    }
+					alert(`Welcome back ${payload.name}`);
 
-    axios.post('/public/login', values);
-  };
+					setValues({
+						email: '',
+						password: '',
+					});
 
-  return (
-    <form onSubmit={onSubmit}>
-      <div className="row">
-        <div className="col-12 mb-30">
-          <Input
-            name="email"
-            value={values.email}
-            type="text"
-            placeholder="you@example.com"
-            id="login-email"
-            onChange={onChange}
-          />
-          {errors.email ? (
-            <Label
-              useFor="validation_error"
-              className="list-group-item-danger"
-              htmlFor="login-email"
-            >
-              {errors.email}
-            </Label>
-          ) : null}
-        </div>
-        <div className="col-12 mb-30">
-          <Input
-            name="password"
-            value={values.password}
-            type="password"
-            placeholder="Password"
-            id="login-password"
-            onChange={onChange}
-          />
-          {errors.password ? (
-            <Label
-              useFor="validation_error"
-              className="list-group-item-danger"
-              htmlFor="login-password"
-            >
-              {errors.password}
-            </Label>
-          ) : null}
-        </div>
-        <div className="col-12 mb-30">
-          <ul>
-            <li>
-              <Input type="checkbox" id="login_remember" checked={isChecked} onChange={check} />
-              <Label htmlFor="login_remember">Remember me</Label>
-            </li>
-          </ul>
-        </div>
-        <div className="col-12 mb-30">
-          <Button testId="login-button" textContent="Login" submit={true} />
-        </div>
+					localStorage.setItem('jwtToken', token);
 
-        <div className="col-12 d-flex justify-content-between">
-          <span>
-            New User to Diaspora Invest?&nbsp;
-            <p className="register-toggle" data-toggle="tab">
-              Register!
-            </p>
-          </span>
-          <span>
-            <Link to="/forgot-password">Forgot Password ?</Link>
-          </span>
-        </div>
-      </div>
-    </form>
-  );
+					const decodedToken = jwtDecode(localStorage.getItem('jwtToken'));
+					// console.log(decodedToken);
+
+					setErrors({});
+				}
+			})
+			.catch(err => {
+				return err;
+			});
+	};
+
+	return (
+		<form onSubmit={onSubmit}>
+			<div className='row'>
+				<div className='col-12 mb-30'>
+					<Input
+						name='email'
+						value={values.email}
+						type='text'
+						placeholder='you@example.com'
+						id='login-email'
+						onChange={onChange}
+					/>
+					{errors.email ? (
+						<Label useFor='validation_error' className='list-group-item-danger' htmlFor='login-email'>
+							{errors.email}
+						</Label>
+					) : null}
+				</div>
+				<div className='col-12 mb-30'>
+					<Input
+						name='password'
+						value={values.password}
+						type='password'
+						placeholder='Password'
+						id='login-password'
+						onChange={onChange}
+					/>
+					{errors.password ? (
+						<Label useFor='validation_error' className='list-group-item-danger' htmlFor='login-password'>
+							{errors.password}
+						</Label>
+					) : null}
+				</div>
+				<div className='col-12 mb-30'>
+					<Button testId='login-button' textContent='Login' submit={true} />
+				</div>
+
+				<div className='col-12 d-flex justify-content-between'>
+					<span>
+						New User to Diaspora Invest?&nbsp;
+						<a className='register-toggle' href='#register-tab'>
+							Register!
+						</a>
+					</span>
+					<span>
+						<a href='forgot-password.html'>Forgot Password ?</a>
+					</span>
+				</div>
+			</div>
+		</form>
+	);
 }
