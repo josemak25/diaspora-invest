01db5813f9db05105be64af806bb02d4b9632280
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-09-18 15:40:10 +0100

    Refactored UserProfile comp to use Input & Button comp

--- src/pages/account/User-Profile.js
+++ src/pages/account/User-Profile.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Input } from '../../components/Input';
+import Button from '../../components/Button';
 
-const UserProfile = () => {
+const UserProfile = ({ details }) => {
   return (
     <>
       <div id='profile-tab' class='tab-pane show active'>
@@ -9,151 +11,32 @@ const UserProfile = () => {
             <div class='col-12 mb-30'>
               <h3 class='mb-0'>Personal Profile</h3>
             </div>
-            <div class='col-md-6 col-12 mb-30'>
-              <label for='f_name'>First Name</label>
-              <input type='text' id='f_name' value='Donald' />
-            </div>
-            <div class='col-md-6 col-12 mb-30'>
-              <label for='l_name'>Last Name</label>
-              <input type='text' id='l_name' value='Palmer' />
-            </div>
             <div class='col-12 mb-30'>
-              <label for='about_me'>About Me</label>
-              <textarea id='about_me'></textarea>
+              <label for='name'>Name</label>
+              <Input type='text' id='name' value={details.name} />
             </div>
             <div class='col-12'>
               <div class='row'>
                 <div class='col-md-6 col-12 mb-30'>
-                  <label for='personal_address'>Address</label>
-                  <input
+                  <label for='personal_email'>Email Address</label>
+                  <Input
                     type='text'
-                    id='personal_address'
-                    value='256, 1st AVE, Manchester 125 , Noth England'
+                    id='personal_email'
+                    value={details.email}
                   />
                 </div>
                 <div class='col-md-6 col-12 mb-30'>
                   <label for='personal_number'>Phone Number</label>
-                  <input
+                  <Input
                     type='text'
                     id='personal_number'
-                    value='(756) 447 5779'
-                  />
-                </div>
-                <div class='col-md-6 col-12 mb-30'>
-                  <label for='personal_email'>Email</label>
-                  <input
-                    type='text'
-                    id='personal_email'
-                    value='info@example.com'
-                  />
-                </div>
-                <div class='col-md-6 col-12 mb-30'>
-                  <label for='personal_web'>Website</label>
-                  <input
-                    type='text'
-                    id='personal_web'
-                    value='www.example.com'
-                  />
-                </div>
-                <div class='col-md-6 col-12 mb-30'>
-                  <label for='personal_agency'>Agencies</label>
-                  <input
-                    type='text'
-                    id='personal_agency'
-                    value=' Royao Estates, Duplex Estates'
-                  />
-                </div>
-                <div class='col-md-6 col-12 mb-30'>
-                  <label for='personal_company'>Company</label>
-                  <input type='text' id='personal_company' value='GTA5' />
-                </div>
-              </div>
-              <h4>Social</h4>
-              <div class='row'>
-                <div class='col-md-6 col-12 mb-30'>
-                  <label for='personal_social_facebook'>
-                    <i class='fa fa-facebook-official'></i>Facebook
-                  </label>
-                  <input
-                    type='text'
-                    id='personal_social_facebook'
-                    value='www.facebook.com'
-                  />
-                </div>
-                <div class='col-md-6 col-12 mb-30'>
-                  <label for='personal_social_twitter'>
-                    <i class='fa fa-twitter'></i>Twitter
-                  </label>
-                  <input
-                    type='text'
-                    id='personal_social_twitter'
-                    value='www.twitter.com'
-                  />
-                </div>
-                <div class='col-md-6 col-12 mb-30'>
-                  <label for='personal_social_linkedin'>
-                    <i class='fa fa-linkedin'></i>Linkedin
-                  </label>
-                  <input
-                    type='text'
-                    id='personal_social_linkedin'
-                    value='www.linkedin.com'
-                  />
-                </div>
-                <div class='col-md-6 col-12 mb-30'>
-                  <label for='personal_social_google'>
-                    <i class='fa fa-google'></i>Google Plus
-                  </label>
-                  <input
-                    type='text'
-                    id='personal_social_google'
-                    value='www.google.com'
-                  />
-                </div>
-                <div class='col-md-6 col-12 mb-30'>
-                  <label for='personal_social_instagram'>
-                    <i class='fa fa-instagram'></i>Instagram
-                  </label>
-                  <input
-                    type='text'
-                    id='personal_social_instagram'
-                    value='www.instagram.com'
-                  />
-                </div>
-                <div class='col-md-6 col-12 mb-30'>
-                  <label for='personal_social_pinterest'>
-                    <i class='fa fa-pinterest'></i>Pinterest
-                  </label>
-                  <input
-                    type='text'
-                    id='personal_social_pinterest'
-                    value='www.pinterest.com'
-                  />
-                </div>
-                <div class='col-md-6 col-12 mb-30'>
-                  <label for='personal_social_skype'>
-                    <i class='fa fa-skype'></i>Skype
-                  </label>
-                  <input
-                    type='text'
-                    id='personal_social_skype'
-                    value='www.skype.com'
-                  />
-                </div>
-                <div class='col-md-6 col-12 mb-30'>
-                  <label for='personal_social_tumblr'>
-                    <i class='fa fa-tumblr'></i>Tumblr
-                  </label>
-                  <input
-                    type='text'
-                    id='personal_social_tumblr'
-                    value='www.tumblr.com'
+                    value={details.phone}
                   />
                 </div>
               </div>
             </div>
             <div class='col-12 mb-30'>
-              <button class='btn'>Save Change</button>
+              <Button textContent='Save Changes' submit='submit' />
             </div>
           </div>
         </form>
