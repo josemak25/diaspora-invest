8678aa82904d44092704b13b164d86a8670b32f2
Nathan-E <eziokwubunduchibueze.com>
2019-10-07 16:06:28 +0100

    validate property upload form

--- src/redux/actions/add-properties.action.js
+++ src/redux/actions/add-properties.action.js
@@ -8,9 +8,10 @@ import { SET_CATEGORY, SET_PROPERTIES, PROPERTY_LOADING } from "../types";
 
 export const uploadProperty = propertyValues => async dispatch => {
   try {
-    console.log(propertyValues)
-    const res = await axios.post(`${process.env.REACT_APP_ENDPOINT_URL}property/create`, propertyValues, SupportHeader());
-    console.log(res);
+    dispatch({type: PROPERTY_LOADING, payload: true})
+    const res = await axios.post(`${process.env.REACT_APP_ENDPOINT_URL}/property/create`, propertyValues, SupportHeader());
+    console.log(res.data);
+    dispatch({ type: PROPERTY_LOADING, payload: false });
   } catch (error) {
     console.log(error);
   }
