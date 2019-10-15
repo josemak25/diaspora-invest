bbe5e0c830c5249e549dc60a77bacdcd3b3be774
Nathan-E <eziokwubunduchibueze.com>
2019-10-15 13:02:47 +0100

    refactor form validator

--- src/utils/editProfile.js
+++ src/utils/editProfile.js
@@ -1,3 +1,5 @@
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 export const validateProfileFields = values => {
   const errorFields = {};
 
@@ -11,7 +13,12 @@ export const validateProfileFields = values => {
   return errorFields;
 };
 
-export const validateAgencyProfileFields = values => {
+export const validateAgencyProfileFields = (
+  values,
+  email = undefined,
+  images = undefined
+) => {
+  console.log(values, email, images);
   const errorFields = {};
 
   if (!values.business_name) {
@@ -27,7 +34,15 @@ export const validateAgencyProfileFields = values => {
   }
 
   if (!values.phone && values.phone.length < 11) {
-    errorFields.phone = "Phone is required";
+    errorFields.phone = "A valid phone number is required";
+  }
+
+  if (email != undefined && !emailRegex.test(email)) {
+    errorFields.email = "A valid email is required";
+  }
+
+  if (images != undefined && images.length < 2) {
+    errorFields.images = "Please upload all required documents";
   }
 
   return errorFields;
