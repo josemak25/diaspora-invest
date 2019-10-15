8bff8967fce4c72e68bd52e64d9b618cbc58b66f
josemak25 <amakirij@gmail.com>
2019-10-15 12:51:23 +0100

    add slider color and background

--- src/components/PriceRange.js
+++ src/components/PriceRange.js
@@ -1,35 +1,84 @@
 import React from 'react';
+import { Slider, Rail, Handles, Tracks } from 'react-compound-slider';
+import { Handle, Track, TooltipRail } from './RangeComponents';
 
-const PriceRange = () => {
-  return (
-    <div>
-      <div>
-        <div
-          id="search-price-range"
-          className="ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content"
-        >
-          <div
-            className="ui-slider-range ui-corner-all ui-widget-header"
-            style={{ left: '12.5%', width: '42.43%' }}
-          ></div>
-          <span
-            tabIndex="0"
-            className="ui-slider-handle ui-corner-all ui-state-default"
-            style={{ left: '12.5%' }}
-          >
-            <span>$12500</span>
-          </span>
-          <span
-            tabIndex="0"
-            className="ui-slider-handle ui-corner-all ui-state-default"
-            style={{ left: '54.93%' }}
-          >
-            <span>$54930</span>
-          </span>
-        </div>
-      </div>
-    </div>
-  );
+const sliderStyle = {
+  position: 'relative',
+  width: '100%'
 };
+
+const defaultValues = [50000000, 500000000];
+
+class PriceRange extends React.Component {
+  state = {
+    domain: [10000000, 1000000000],
+    values: defaultValues.slice(),
+    update: defaultValues.slice(),
+    reversed: false
+  };
+
+  onUpdate = update => {
+    this.setState({ update });
+  };
+
+  onChange = values => {
+    this.setState({ values });
+  };
+
+  setDomain = domain => {
+    this.setState({ domain });
+  };
+
+  toggleReverse = () => {
+    this.setState(prev => ({ reversed: !prev.reversed }));
+  };
+
+  render() {
+    const {
+      state: { domain, values, reversed }
+    } = this;
+
+    return (
+      <div style={{ height: 10, width: '100%' }}>
+        <Slider
+          mode={3}
+          step={20}
+          domain={domain}
+          reversed={reversed}
+          rootStyle={sliderStyle}
+          onUpdate={this.onUpdate}
+          onChange={this.onChange}
+          values={values}
+        >
+          <Rail>{railProps => <TooltipRail {...railProps} />}</Rail>
+          <Handles>
+            {({ handles, activeHandleID, getHandleProps }) => (
+              <div className="slider-handles">
+                {handles.map(handle => (
+                  <Handle
+                    key={handle.id}
+                    handle={handle}
+                    domain={domain}
+                    isActive={handle.id === activeHandleID}
+                    getHandleProps={getHandleProps}
+                  />
+                ))}
+              </div>
+            )}
+          </Handles>
+          <Tracks left={false} right={false}>
+            {({ tracks, getTrackProps }) => (
+              <div className="slider-tracks">
+                {tracks.map(({ id, source, target }) => (
+                  <Track key={id} source={source} target={target} getTrackProps={getTrackProps} />
+                ))}
+              </div>
+            )}
+          </Tracks>
+        </Slider>
+      </div>
+    );
+  }
+}
 
 export default PriceRange;
