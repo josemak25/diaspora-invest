e0db21219b8376c7b5b33e4e0056366678f880fc
Nathan-E <eziokwubunduchibueze.com>
2019-10-15 13:07:15 +0100

    refactor code

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import { isUserType } from '../utils/roles';
-import Header from '../common/header/Header';
-import HomePage from '../pages/homepage/HomePage';
-import Account from '../pages/account/Account';
-import Property from '../pages/view-property/Property';
-import Properties from '../pages/properties/Properties';
-import LoginSignup from '../pages/login-signup/Login-SignUp';
-import AddProperties from '../pages/add-property/AddProperty';
-import AdminDashboard from '../pages/admin-dashboard/AdminDashboard';
-import SellerSetup from '../pages/account-setup/AccountSetup';
+import { isUserType } from "../utils/roles";
+import Header from "../common/header/Header";
+import HomePage from "../pages/homepage/HomePage";
+import Account from "../pages/account/Account";
+import Property from "../pages/view-property/Property";
+import Properties from "../pages/properties/Properties";
+import LoginSignup from "../pages/login-signup/Login-SignUp";
+import AddProperties from "../pages/add-property/AddProperty";
+import AdminDashboard from "../pages/admin-dashboard/AdminDashboard";
+import SellerSetup from "../pages/account-setup/AccountSetup";
 
 function PrivateRoute({ component: Component, hasValidAccess, ...rest }) {
   return (
@@ -25,7 +30,7 @@ function PrivateRoute({ component: Component, hasValidAccess, ...rest }) {
             <Component {...props} />
           </>
         ) : (
-          <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+          <Redirect to={{ pathname: "/", state: { from: props.location } }} />
         )
       }
     />
@@ -37,13 +42,13 @@ function PublicRoute({ component: Component, hasValidAccess, ...rest }) {
     <Route
       {...rest}
       render={props =>
-        !hasValidAccess || Component.name === 'HomePage' ? (
+        !hasValidAccess || Component.name === "HomePage" ? (
           <>
             <Header />
             <Component {...props} />
           </>
         ) : (
-          <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+          <Redirect to={{ pathname: "/", state: { from: props.location } }} />
         )
       }
     />
@@ -55,12 +60,12 @@ function AdminRoute({ component: Component, hasValidAccess, ...rest }) {
     <Route
       {...rest}
       render={props =>
-        hasValidAccess && isUserType('admin') ? (
+        hasValidAccess && isUserType("admin") ? (
           <>
             <Component {...props} />
           </>
         ) : (
-          <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+          <Redirect to={{ pathname: "/", state: { from: props.location } }} />
         )
       }
     />
@@ -72,7 +77,12 @@ function Main({ authenticated }) {
     <main>
       <Router>
         <Switch>
-          <PublicRoute hasValidAccess={authenticated} exact path="/" component={HomePage} />
+          <PublicRoute
+            hasValidAccess={authenticated}
+            exact
+            path="/"
+            component={HomePage}
+          />
           <PublicRoute
             hasValidAccess={authenticated}
             exact
@@ -85,7 +95,12 @@ function Main({ authenticated }) {
             path="/account-setup"
             component={SellerSetup}
           />
-          <PrivateRoute hasValidAccess={authenticated} exact path="/profile" component={Account} />
+          <PrivateRoute
+            hasValidAccess={authenticated}
+            exact
+            path="/profile"
+            component={Account}
+          />
           <PrivateRoute
             hasValidAccess={authenticated}
             exact
@@ -104,7 +119,11 @@ function Main({ authenticated }) {
             path="/add-properties"
             component={AddProperties}
           />
-          <AdminRoute hasValidAccess={authenticated} path="/dashboard" component={AdminDashboard} />
+          <AdminRoute
+            hasValidAccess={authenticated}
+            path="/dashboard"
+            component={AdminDashboard}
+          />
         </Switch>
       </Router>
     </main>
